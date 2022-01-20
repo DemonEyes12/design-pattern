e8f8538aabbf4bd00a3bbc75ccf6daac5c28e4f1
DemonEyes12 <anemnonov2@gmail.com>
2022-01-20 08:53:52 +0300

    Добавьте файлы проекта.

---
+++ myconsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mylibrary;
+
+namespace myconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck[] ducks = new Duck[]
+            {
+                new MallardDuck(),
+                new RedheadDuck()
+            };
+
+            foreach (Duck duck in ducks)
+            {
+                Console.WriteLine(duck.Quack());
+                Console.WriteLine(duck.Swim());
+                Console.WriteLine(duck.Display());
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
